e4eb6b10c573ede573605083efc88aeb20d5cf2b
Erin Dawson <erinedawson@gmail.com>
2020-02-14 13:03:22 -0700

    enqueue main.css

--- sandboxtheme/functions.php
+++ sandboxtheme/functions.php
@@ -2,6 +2,7 @@
 
 function add_my_styles(){
     wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_style('main', get_template_directory_uri() . '/main.css');
 }
 
 add_action('wp_enqueue_scripts', 'add_my_styles');
