69be89d51a0e58d02c1a883e4cb127cd22e45a11
Erin Dawson <erinedawson@gmail.com>
2020-02-14 16:50:11 -0700

    add linking buttons to remaining modules

--- sandboxtheme/single-clients.php
+++ sandboxtheme/single-clients.php
@@ -29,18 +29,27 @@ get_header();
          <div class="col-sm">
              <div class="card">
                  <h3>Site Design</h3>
+                 <button type="button" class="btn btn-sm btn-info">
+                     <a class="cardlinks" href="<?php the_field('site_design'); ?>">Click Through.</a>
+                 </button>
              </div>
 
          </div>
          <div class="col-sm">
              <div class="card">
                  <h3>Timeline</h3>
+                 <button type="button" class="btn btn-sm btn-info">
+                     <a class="cardlinks" href="<?php the_field('timeline'); ?>">Click Through.</a>
+                 </button>
              </div>
 
          </div>
          <div class="col-sm">
              <div class="card">
                  <h3>Web Development</h3>
+                 <button type="button" class="btn btn-sm btn-info">
+                     <a class="cardlinks" href="<?php the_field('web_development'); ?>">Click Through.</a>
+                 </button>
              </div>
 
          </div>
