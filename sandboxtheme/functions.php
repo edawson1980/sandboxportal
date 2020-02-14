b77bab1bd09680fef82e4997a804ef4b39c71a5d
Erin Dawson <erinedawson@gmail.com>
2020-02-14 13:13:22 -0700

    move linked stylesheets to enqueue function

--- sandboxtheme/functions.php
+++ sandboxtheme/functions.php
@@ -3,6 +3,7 @@
 function add_my_styles(){
     wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_style('main', get_template_directory_uri() . '/main.css');
+    wp_enqueue_style('bootstrap_css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
 }
 
 add_action('wp_enqueue_scripts', 'add_my_styles');
