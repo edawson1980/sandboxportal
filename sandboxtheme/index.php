1e44465b8b954683995653b35053340cc70574cb
Erin Dawson <erinedawson@gmail.com>
2020-02-14 15:49:44 -0700

    add Loop to index.php

--- sandboxtheme/index.php
+++ sandboxtheme/index.php
@@ -3,4 +3,15 @@
 <div class="main">
     <h1>This is the page title.</h1>
 
+    <!-- The Loop -->
+    <?php
+    if( have_posts() ) :
+        while ( have_posts() ) : the_post();
+
+        endwhile;
+    endif;
+
+     ?>
+
+
 <?php get_footer(); ?>
