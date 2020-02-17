b478c1604e2805261817b49658b2313de90a478f
Erin Dawson <erinedawson@gmail.com>
2020-02-17 16:05:24 -0700

    Create single-sitecontent.php

---
+++ sandboxtheme/single-sitecontent.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Template Name: Single Client Template
+Description: Layout for individual Clients dashboard
+*/
+get_header();
+ ?>
+
+ <div class="container">
+     <div class="row">
+         <div class="dashtitle">
+             <h1>Welcome, <?php the_field('client_name') ?></h1>
+         </div>
+     </div>
+
+     <div class="row">
+         <!-- start the Loop -->
+         <?php while( have_posts() ) : the_post(); ?>
+
+         <div class="col-sm">
+             <div class="card">
+                 <h3>Site Content</h3>
+                 <button type="button" class="btn btn-sm btn-info">
+                     <a class="cardlinks" href="<?php the_field('site_content'); ?>">Click Through.</a>
+                 </button>
+             </div>
+
+         </div>
+         <div class="col-sm">
+             <div class="card">
+                 <h3>Site Design</h3>
+                 <button type="button" class="btn btn-sm btn-info">
+                     <a class="cardlinks" href="<?php the_field('site_design'); ?>">Click Through.</a>
+                 </button>
+             </div>
+
+         </div>
+         <div class="col-sm">
+             <div class="card">
+                 <h3>Timeline</h3>
+                 <button type="button" class="btn btn-sm btn-info">
+                     <a class="cardlinks" href="<?php the_field('timeline'); ?>">Click Through.</a>
+                 </button>
+             </div>
+
+         </div>
+         <div class="col-sm">
+             <div class="card">
+                 <h3>Web Development</h3>
+                 <button type="button" class="btn btn-sm btn-info">
+                     <a class="cardlinks" href="<?php the_field('web_development'); ?>">Click Through.</a>
+                 </button>
+             </div>
+
+         </div>
+
+      <!-- end the Loop -->
+     <?php endwhile; ?>
+
+     </div>
+
+ </div>
+
+
+
+
+
+
+ <?php get_footer(); ?>
