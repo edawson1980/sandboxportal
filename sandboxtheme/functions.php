2f9051f6162d566551c567c84d21b34a992a44ee
Erin Dawson <erinedawson@gmail.com>
2020-02-19 15:52:12 -0700

    add scripts.js to add_my_styles

--- sandboxtheme/functions.php
+++ sandboxtheme/functions.php
@@ -4,6 +4,7 @@ function add_my_styles(){
     wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_style('main', get_template_directory_uri() . '/main.css');
     wp_enqueue_style('bootstrap_css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+    wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'));
 }
 
 add_action('wp_enqueue_scripts', 'add_my_styles');
