4ab7521abcb38c6ddbe71a8ef7c78854beda9dd4
Erin Dawson <erinedawson@gmail.com>
2020-02-19 16:53:03 -0700

    Update scripts.js

--- sandboxtheme/js/scripts.js
+++ sandboxtheme/js/scripts.js
@@ -1,8 +1,8 @@
 // use jQuery to remove footer from Google Docs iframe
 jQuery(document).ready(function($){
-    var head = jQuery("#iframe").contents().find("head");
+    var head = $("iframe").contents().find("head");
     var css = '<style type="text/css" >' +
               '#header{display:none};' +
               '</style>';
-    jQuery(head).append(css);
+    $(head).append(css);
 });
