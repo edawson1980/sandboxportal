e6e76f8994d2a3ee5dbe992652dfb43db5a0a4ac
Erin Dawson <erinedawson@gmail.com>
2020-02-21 09:31:25 -0700

    run jquery function test, remove iframe css script

--- sandboxtheme/js/scripts.js
+++ sandboxtheme/js/scripts.js
@@ -1,8 +1,12 @@
 // use jQuery to remove footer from Google Docs iframe
 jQuery(document).ready(function($){
-    var head = $("iframe").contents().find("head");
-    var css = '<style type="text/css" >' +
-              '#header{display:none};' +
-              '</style>';
-    $(head).append(css);
+    // var head = $("p iframe").contents().find("head");
+    // var css = '<style type="text/css" >' +
+    //           '#header{display:none};' +
+    //           '</style>';
+    // $(head).append(css);
+
+    $('.dashtitle').mouseover(function(){
+        $('.dashtitle').hide();
+    });
 });
