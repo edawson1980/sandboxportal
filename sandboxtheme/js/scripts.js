1b4b4be9c6374ca5149163b35faa0d65c63d5b10
Erin Dawson <erinedawson@gmail.com>
2020-02-21 10:44:27 -0700

    disable round 1 radio button after click

--- sandboxtheme/js/scripts.js
+++ sandboxtheme/js/scripts.js
@@ -1,5 +1,8 @@
-// use jQuery to remove footer from Google Docs iframe
+// use jQuery to disable Revision button after it's been selected
 jQuery(document).ready(function($){
 
+    $("input[value='one']").click(function(){
+        $("input[value='one']").prop("disabled",true);
+    });
 
 });
