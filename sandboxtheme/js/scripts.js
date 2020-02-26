a87e2b07a68f56bc1705cbea29c6f2953d347cd9
Erin Dawson <erinedawson@gmail.com>
2020-02-26 11:45:56 -0700

    inelegant, temporary solution
     allows for Rounds data to persist and for Rounds buttons to be 'disabled'.

--- sandboxtheme/js/scripts.js
+++ sandboxtheme/js/scripts.js
@@ -1,27 +1,51 @@
 jQuery(document).ready(function($){
 
-
 // use jQuery to disable Revision button after it's been selected
-    if($("input[value='one']:checked")){
-        $("input[value='one']:checked").prop("disabled",true);
-    };
-
-    if($("input[value='two']:checked")){
-        $("input[value='two']:checked").prop("disabled",true);
-    };
-
-    if($("input[value='three']:checked")){
-        $("input[value='three']:checked").prop("disabled",true);
-    };
+    // if($("input[value='one']:checked")){
+    //     $("input[value='one']:checked").prop("disabled",true);
+    // };
 
 
-    //prevent form submission once:
-    stopSend function(){
-        $("input[value='submitfeedback']").addEventListener("click", function(event){
-            event.preventDefault();
-            alert('form submission stopped');
-        });
-    }
+        // $("input[value='one']").click(function(){
+        //     if($("input[value='one']:checked")){
+        //         $("input[value='one']:checked").prop("disabled",true);
+        //     }
+        // });
+
+        // if($("input[value='one']:checked")){
+        //     $(".selected").hide();
+        // }
+
+        //TEMPORARY WORKAROUND
+        if($("input[value='one']:checked")){
+            $("input[value='one']:checked").hide();
+        }
+        if($("input[value='two']:checked")){
+            $("input[value='two']:checked").hide();
+        }
+        if($("input[value='three']:checked")){
+            $("input[value='three']:checked").hide();
+        }
+
+
+    // if($("input[value='two']:checked")){
+    //     $("input[value='two']:checked").prop("disabled",true);
+    // };
+    //
+    // if($("input[value='three']:checked")){
+    //     $("input[value='three']:checked").prop("disabled",true);
+    // };
+
+
+        //prevent form submission once:
+        // stopSend function(){
+        //     $("input[value='submitfeedback']").addEventListener("click", function(event){
+        //         event.preventDefault();
+        //
+        //         alert('form submission stopped');
+        //     });
+        // }
+
 
 
 
