7a7a8d471452f0194f06d6d13d1d3ef534d3aecb
Erin Dawson <erinedawson@gmail.com>
2020-02-26 12:55:06 -0700

    Update single-sitecontent.php
    hide $wpdb->update until figure out how to limit number in array.

--- sandboxtheme/single-sitecontent.php
+++ sandboxtheme/single-sitecontent.php
@@ -19,7 +19,7 @@ get_header();
      <div class="container-fluid">
          <div class="row">
              <!-- start the Loop -->
-             <?php while( have_posts() ) : the_post(); ?>
+             <!-- <?php while( have_posts() ) : the_post(); ?> -->
 
              <div class="col-md-12">
                  <div class="dashtitle">
@@ -29,8 +29,8 @@ get_header();
                      <p><span style="color:red;">Important:</span> Each proposal includes three rounds of edits per deliverable.  Brand Iron is happy to provide additional work after all rounds of edits have been used, at a rate of $275/hour.  With this in mind, we strongly suggest you double-check your feedback before clicking the 'Submit Feedback' button.</p>
                  </div>
                  <div class="downloadarea">
-                     <p>Click <a href="<?php the_field('sc_download'); ?>" target="_blank">here</a> for the most recent version of your website content.</p>
                      <p><?php the_field('sc_doc_embed'); ?></p>
+                     <p>Click <a href="<?php the_field('sc_download'); ?>" target="_blank">here</a> for the most recent PRINTABLE version of your website content.</p>
                  </div>
                      <div class="card acf-form">
                          <?php acf_form(array(
@@ -49,22 +49,16 @@ get_header();
                      </div>
 
                      <?php
-                        //list the revisions values, push them to db
-                        //user input:
-                        // $rounds = get_field('field_5e4b19bb2ff1c');
-
+                        //combine previous revisions values with new revisions values, push them all to db
 
                         //step 1: query the database to find the entry for this field.
                         $choice = $wpdb->get_var("SELECT meta_value FROM wp_postmeta WHERE meta_key = 'sc_revisions'");
 
-                        //step 2: loop through the checkboxes to see if any have been checked.  if so, grab that data.
+                        //step 2: grab the current user data
 
                         $rounds = get_field('field_5e4b19bb2ff1c');
 
-
-
-
-                        //step 3: combine the queried value with the new user-generated value
+                        //step 3: combine the queried value with the new user-generated value:
 
                         //convert $choice (which comes in as a serialized string) back into an array
                         $choicearray = unserialize($choice);
@@ -79,20 +73,31 @@ get_header();
                         //step 4: update the db
                         //from L-R once inside the update helper function:
                         //postmeta = the table, meta_value = the column (fat arrow indicates what to update that column's value with), meta_id = Location to perform updates (triangulate with column intersecting with which row.  in other words, find the appropriate cell).
-                        $wpdb->update($wpdb->postmeta, array('meta_value' => serialize($updatedround)), array('meta_id' => 20));
+                        // $wpdb->update($wpdb->postmeta, array('meta_value' => serialize($updatedround)), array('meta_id' => 20));
 
                         //step 5: reset the query to only ever have three elements, max:
 
-
                       ?>
-
-
 
              </div>
 
-          <!-- end the Loop -->
-         <?php endwhile; ?>
 
+          <!-- end the Loop -->
+         <!-- <?php endwhile; ?> -->
+
+
+
+
+         </div>
+         <!-- back to Dashboard -->
+         <div class="row">
+             <div class="backto">
+                 test test
+                 <p>testing</p>
+
+
+
+            </div>
          </div>
      </div>
 
