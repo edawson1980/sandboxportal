9dd0591a9677ec1aa073fe437b9e7817f5751955
Erin Dawson <erinedawson@gmail.com>
2020-02-27 09:42:30 -0700

    add explanation of why no registration option.

--- sandboxtheme/login.php
+++ sandboxtheme/login.php
@@ -2,6 +2,8 @@
 /*
 Template Name: Login Page Template
 Description: Login area for Clients
+
+**no need to create registration functionality. this will be handled in the back end by the Agency.
 */
 
 get_header();
